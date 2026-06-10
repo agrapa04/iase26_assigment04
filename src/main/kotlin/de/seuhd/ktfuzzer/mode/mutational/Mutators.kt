60a0ed823aac0a00358af92f1976770273ceb28b
Adrienne <adrienne.grapa@gmail.com>
2026-06-10 13:31:08 +0200

    4.1 a) b)

--- src/main/kotlin/de/seuhd/ktfuzzer/mode/mutational/Mutators.kt
+++ src/main/kotlin/de/seuhd/ktfuzzer/mode/mutational/Mutators.kt
@@ -10,17 +10,38 @@ import kotlin.random.Random
 internal object Mutators {
     /** Deletes one randomly chosen character. */
     fun deleteRandomCharacter(input: String, random: Random): String =
-        TODO("Exercise 1: delete one randomly chosen character")
+        run {
+            if (input.isEmpty()) return input
+            val index = random.nextInt(input.length)
+            return input.removeRange(index, index + 1)
+        }
 
     /** Inserts one character drawn uniformly from [alphabet] at a random position. */
     fun insertRandomCharacter(input: String, alphabet: List<Char>, random: Random): String =
-        TODO("Exercise 1: insert one character from the alphabet at a random position")
+        run {
+            val char = alphabet[random.nextInt(alphabet.size)]
+            val index = random.nextInt(input.length + 1)
+            return input.substring(0, index) + char + input.substring(index)
+        }
 
     /** Flips one randomly chosen low bit of one randomly chosen character. */
     fun flipRandomCharacter(input: String, random: Random): String =
-        TODO("Exercise 1: flip one random low bit of one randomly chosen character")
+        run {
+            if (input.isEmpty()) return input
+            val index = random.nextInt(input.length)
+            val char = input[index]
+            val bitToFlip = 1 shl random.nextInt(7)
+            val flippedChar = char.code xor bitToFlip
+            return input.substring(0, index) + flippedChar.toChar() + input.substring(index+1)
+        }
 
     /** Repeats one randomly chosen character a random number of times in place. */
     fun repeatRandomCharacter(input: String, random: Random): String =
-        TODO("Exercise 1: repeat one randomly chosen character a random number of times")
+        run {
+            if (input.isEmpty()) return input
+            val index = random.nextInt(input.length)
+            val char = input[index]
+            val repeatCount = random.nextInt(2, 5) // Repeat the character
+            return input.substring(0, index) + char.toString().repeat(repeatCount) + input.substring(index + 1)
+        }
 }
